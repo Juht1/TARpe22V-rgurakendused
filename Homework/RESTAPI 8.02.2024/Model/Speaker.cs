03eec975c69600d0e6e1165d84f573f5be8ce496
Juht1 <kristjan.juht@gmail.com>
2024-02-08 11:35:43 +0200

    A

---
+++ Homework/RESTAPI 8.02.2024/Model/Speaker.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ITB2203Application.Model;
+
+public class Speaker
+{
+    public string name {get; set;}
+    public int id {get; set;}
+    public string Email {get; set;}
+}
